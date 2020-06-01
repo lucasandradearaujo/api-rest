d82155af631b6956c6e1bd3ce69808625cb5fb5b
Lucas Andrade <lucasaraujo@si.fiponline.edu.br>
2020-05-31 23:17:05 -0300

    pt.2 atividade

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# api-rest
+# apirest
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.listen(3000);
+
+require('./app/controllers/index')(app);
+
+app.listen(3000)
